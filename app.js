f89524c764421007cc87359f84bcbc312319601d
Arnav Singh <arnavsingh1359@gmail.com>
2022-12-02 01:06:03 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector("#mobile-menu");
+const menuList = document.querySelector(".navbar-menu");
+
+menu.addEventListener("click",function() {
+    menu.classList.toggle("is-active");
+})
